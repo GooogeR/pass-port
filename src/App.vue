45b219f17ff8ce684d674778d15809d3c76a407c
glimmerald <glimmerald@gmail.com>
2024-06-12 23:31:29 +0300

    WIP

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,78 @@
 <template>
-  test
-  <div>
+  <section class="intro-section">
+    <div class="container">
+      <div class="like">
+        <div class="like-title">Всё понравилось?</div>
+        <div class="stars">
+          <img src="../src/assets/Star.svg" alt="star">
+          <img src="../src/assets/Star.svg" alt="star">
+          <img src="../src/assets/Star.svg" alt="star">
+          <img src="../src/assets/Star.svg" alt="star">
+          <img src="../src/assets/Star.svg" alt="star">
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="main-section">
+    <div class="top-bridge"></div>
+    <div class="container">
+      <div class="company-name-container">
+        <div class="company-logo"></div>
+        <p class="company-name">Автомойка Кирова, 12а</p>
+      </div>
+      <div class="card">
+        <h3 class="card-title">Всё понравилось?</h3>
+        <p class="card-semi-title">Оставьте отзыв</p>
+        <div class="card-links-container">
+          <a href="#" class="card-link">
+            <img src="../src/assets/app-icon.png" alt="" class="card-link-img">
+            <p class="card-link-text">Яндекс Карты</p>
+          </a>
+          <a href="#" class="card-link">
+            <img src="#" alt="" class="card-link-img">
+            <p class="card-link-text">2Gis</p>
+          </a>
+          <a href="#" class="card-link">
+            <img src="#" alt="" class="card-link-img">
+            <p class="card-link-text">Google Карты</p>
+          </a>
+        </div>
+      </div>
+      <div class="card">
+        <h3 class="card-title">Чаевые</h3>
+        <p class="card-semi-title">Обрадовать коллектив</p>
+        <div class="card-links-container">
+          <a href="#" class="card-link">
+            <img src="#" alt="" class="card-link-img">
+            <p class="card-link-text">нетмонет</p>
+          </a>
+        </div>
+      </div>
+      <div class="card">
+        <h3 class="card-title">Недовольны?</h3>
+        <p class="card-semi-title">Напишите нам</p>
+        <div class="card-links-container">
+          <a href="#" class="card-link">
+            <img src="#" alt="" class="card-link-img">
+            <p class="card-link-text">WhatsApp</p>
+          </a>
+          <a href="#" class="card-link">
+            <img src="#" alt="" class="card-link-img">
+            <p class="card-link-text">Telegram</p>
+          </a>
+        </div>
+      </div>
+
+    </div>
+    <div class="bottom-bridge"></div>
+  </section>
+  <section class="end-section">
+    <div class="container">
+      <small class="copyrights">pass-port business</small>
+      <p class="slogan">pass-port business — продвижение на картах</p>
+    </div>
+  </section>
+  <!-- <div>
     <h1>Firestore Example</h1>
     <input v-model="newName" placeholder="Name" />
     <input v-model="newEmail" placeholder="Email" />
@@ -13,7 +85,7 @@
         <p><strong>Phone:</strong> {{ item.phone }}</p>
       </li>
     </ul>
-  </div>
+  </div> -->
 </template>
 
 <script>
@@ -36,8 +108,8 @@ export default {
           email: newEmail.value,
           phone: newPhone.value
         });
-        fetchItems(); 
-        newName.value = ''; 
+        fetchItems();
+        newName.value = '';
         newEmail.value = '';
         newPhone.value = '';
       } catch (e) {
@@ -68,3 +140,150 @@ export default {
 };
 </script>
 
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Dela+Gothic+One&family=IBM+Plex+Sans:wght@500&display=swap');
+
+body {
+  margin: 0;
+  font-family: "Dela Gothic One", sans-serif;
+
+}
+
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  max-width: 592px;
+  width: calc(100% - 32px);
+}
+
+.main-section {
+  position: relative;
+  background-color: #DEDEDE;
+
+  .container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    min-width: 100%;
+    gap: 16px;
+  }
+}
+
+.top-bridge {
+  height: 50px;
+  border-radius: 0 0 50px 50px;
+  background-color: #3D6C59;
+  position: absolute;
+  top: -1px;
+  left: 0;
+  width: 100%;
+}
+
+.bottom-bridge {
+  height: 50px;
+  border-radius: 50px 50px 0 0;
+  background-color: #3D6C59;
+  position: absolute;
+  bottom: -1px;
+  left: 0;
+  width: 100%;
+}
+
+.like {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 26px;
+}
+
+.like-title {
+  font-size: 22px;
+  color: #ffffff;
+}
+
+.stars {
+  display: flex;
+  gap: 10px;
+}
+
+.star {
+  width: 57px;
+  height: 57px;
+}
+
+.intro-section {
+  background-color: #3D6C59;
+  height: 900px;
+  position: relative;
+}
+
+.company-name-container {
+  position: absolute;
+  top: -80px;
+  left: 50%;
+  transform: translate(-50%);
+  display: flex;
+  gap: 15px;
+  align-items: center;
+}
+
+.company-logo {
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  background: #ccc;
+}
+
+.company-name {
+  font-size: 15px;
+  color: #fff;
+}
+
+.card {
+  background-color: #fff;
+  border-radius: 20px;
+  z-index: 1;
+  padding: 20px;
+  width: 400px;
+  box-shadow: 10px 10px 30px 10px #3f3f3f56;
+}
+
+.card-links-container {
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+}
+
+.card-link {
+  border: 1px solid #000;
+  border-radius: 15px;
+  display: flex;
+}
+
+.card-title {
+  font-size: 32px;
+  margin: 0;
+}
+
+.card-link-img {
+  width: 30px;
+  height: 30px;
+  object-fit: contain;
+}
+
+.card-semi-title {
+  font-size: 15px;
+  margin: 8px 0 0 0;
+}
+
+.end-section {
+  background-color: #3D6C59;
+  height: 900px;
+
+}
+</style>
